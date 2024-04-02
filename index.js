16910ec01a8c142bbd341feea3775844e8874bcd
jeissonR115 <jeissonfeliper@gmail.com>
2024-04-02 00:32:16 -0500

    add variables in with .env

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { MongoClient, ObjectId } from "mongodb";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-const URL = process.env.DB_URL || '****';
-const dbName = process.env.DB_NAME || 'ambidata';
+const URL = process.env.DB_URL || 'mongodb+srv://***.mongodb.net/';
+const dbName = process.env.DB_NAME || 'db';
 const collections = {
     sensorData: "sensorData",
     users: "users"
@@ -14,7 +17,7 @@ const connect = async (url, dbName) => {
         console.log('Conexión exitosa a MongoDB');
         return client.db(dbName);
     } catch (err) {
-        throw Error('No se ha establecido la conexión a la base de datos');
+        throw Error(`No se ha establecido la conexión a la base de datos ${PORT}`);
     }
 }
 connect(URL,dbName);
