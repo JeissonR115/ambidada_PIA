2282b01ee42079c0ef86b636ccd21a57ad86462b
eragon1020 <brayan.aragon.zabala@gmail.com>
2024-04-11 17:41:09 -0500

    conexion con esp 32 temperature

---
+++ esp32/sketch_apr10a_copy_20240411165032_copy_20240411171139/sketch_apr10a_copy_20240411165032_copy_20240411171139.ino
@@ -0,0 +1,45 @@
+#include <HTTPClient.h>
+#include "DHTesp.h"
+
+int pinDHT = 26;
+DHTesp dht;
+
+void setup() {
+  Serial.begin(115200);
+  dht.setup(pinDHT, DHTesp::DHT11);
+
+  WiFi.begin("F_Villamizar", "Movistar2024");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando a la red Wi-Fi...");
+  }
+
+  Serial.println("Conectado a la red Wi-Fi");
+}
+
+void loop() {
+  TempAndHumidity data = dht.getTempAndHumidity();
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin("https://flv82qmr-3000.use.devtunnels.ms/sensorData/esp32");
+    http.addHeader("Content-Type", "application/json");
+
+  String jsonData = "{\"temperature\":" + String(data.temperature, 2) + ",\"ambient\":" + String(data.humidity, 1) + ",\"place\":\"cascada\"}";
+
+   int httpResponseCode = http.POST(jsonData);
+if (httpResponseCode > 0) {
+  String response = http.getString();
+  Serial.println("Respuesta del servidor: " + response);
+} else {
+  Serial.print("Error en la solicitud al servidor: ");
+  Serial.println(httpResponseCode);
+}
+
+    http.end();
+    
+  }
+
+  delay(2000);
+}
